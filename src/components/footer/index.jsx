f0a7434af8330b1eeaf5fa889158c91cfc16baa0
Vyacheslav <korzan.va@mail.ru>
2022-04-09 21:55:28 +0500

    1 lesson

---
+++ src/components/footer/index.jsx
@@ -0,0 +1,18 @@
+import s from './index.module.css';
+
+const Footer = () => {
+
+  return (
+    <footer className={s.root}>
+      <div className={s.container}>
+        <div className={s.footerWrap}>
+          Coded with
+          <span className={s.heart}></span>
+          by You
+        </div>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer;
